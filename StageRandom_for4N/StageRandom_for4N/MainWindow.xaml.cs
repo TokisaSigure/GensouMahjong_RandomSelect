8b2f1f16bceaa2b0e08dbbdf4b587b43bdf9bc6c
赤木詠滋 <s1223077@cce.kanagawa-it.ac.jp>
2015-10-09 17:47:31 +0900

    firstcommit

---
+++ StageRandom_for4N/StageRandom_for4N/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections;
+using YukariGames;
+
+namespace StageRandom_for4N
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            ArrayList cName = new ArrayList();
+            ArrayList sName = new ArrayList();
+            Random rand = new Random();
+            TextLoadClass textLoadClass = new TextLoadClass();
+            textLoadClass.TextRoad(ref cName, "Character.txt", "UTF-8");
+            textLoadClass.TextRoad(ref sName, "stage.txt", "UTF-8");
+            int cnum = rand.Next(cName.Count-1);
+            int snum = rand.Next(sName.Count-1);
+            this.CharacterName.Text = (string)cName[cnum];
+            this.StageBox.Text = (string)sName[snum];
+        }
+    }
+}
